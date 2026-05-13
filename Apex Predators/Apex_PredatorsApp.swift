276e5dae28efde9642f1dd3f79fc6cacf0cfb89f
akashkumbhar26 <akash.kumbhar@helloteam.com>
2026-05-13 13:02:22 +0530

    Initial Commit

---
+++ Apex Predators/Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apex_PredatorsApp.swift
+//  Apex Predators
+//
+//  Created by Akash Kumbhar on 13/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
